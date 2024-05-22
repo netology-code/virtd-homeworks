f496b820b607effd98f18952e275e53a837c1006
udjin <udjin10@gmail.com>
2024-05-22 15:56:19 +0300

    fix dive docker save

--- 05-virt-04-docker-in-practice/lecture_demonstration/07.dive/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Привет, мир!!!! Multistage в Docker!'
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
