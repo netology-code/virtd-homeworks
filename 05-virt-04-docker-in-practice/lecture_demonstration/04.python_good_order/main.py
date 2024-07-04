10b76df1f17583ea2a75e681994eb272710b99cd
udjin <udjin10@gmail.com>
2024-07-04 18:54:02 +0300

    adds

--- 05-virt-04-docker-in-practice/lecture_demonstration/04.python_good_order/main.py
+++ 05-virt-04-docker-in-practice/lecture_demonstration/04.python_good_order/main.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import os
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Привет, мир!!!! Это мое первое веб-приложение в Docker!'
+    test_string = os.environ.get('TEST_STRING', '')  # Считываем значение переменной среды TEST_STRING
+    return f'Привет, {test_string}! '  # Добавляем значение TEST_STRING в конец строки
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
